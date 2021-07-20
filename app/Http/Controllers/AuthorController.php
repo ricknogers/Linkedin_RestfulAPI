a56435a55b1d53cfbd8e0a4d27fd9c7d9eaa6f61
SNF-RickNogers <nrogers@snf.com>
2021-07-20 14:36:41 -0400

    First Commit

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\AuthorCollection;
+use App\Http\Resources\AuthorResource;
+use App\Models\Author;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     *  @return \App\Http\Resources\AuthorCollection
+     */
+    public function index()
+    {
+        return new AuthorCollection(Author::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Author  $author
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(Author $author)
+    {
+        return response()->json(new AuthorResource($author), Response::HTTP_OK);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Author $author)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Author  $author
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Author $author)
+    {
+        //
+    }
+}
